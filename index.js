6e0087e03b9b3ec1dce98ef64ef507bda21d138d
bruh <89742348+sauc23@users.noreply.github.com>
2024-10-20 09:58:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-
 
 const target = 'http://jmrk.xyz:3050';
 
@@ -12,13 +10,16 @@ const mathProxy = createProxyMiddleware({
   target,
   changeOrigin: true,
   logLevel: 'debug',
+  onProxyReq: function(proxyReq, req, res) {
+    // Set the User-Agent header
+    proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.61 Safari/537.36');
+  },
   onProxyRes: function(proxyRes, req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
   }
 });
-
 
 // Use the proxy middleware for all requests
 app.use('/', mathProxy);
